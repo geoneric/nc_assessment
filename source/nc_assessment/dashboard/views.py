f158f21d9b33b1bef078646b6b215f77b6692e6b
Kor de Jong <k.dejong@geoneric.eu>
2017-01-15 13:06:37 +0100

    Add plans to dashboard

--- source/nc_assessment/dashboard/views.py
+++ source/nc_assessment/dashboard/views.py
@@ -1,37 +1,29 @@
-# import json
 import requests
 from flask import current_app, flash, render_template, url_for
 from . import dashboard
 
 
-# def api_uri():
-#     return "http://{}:{}/{}".format(
-#         current_app.config["NC_ASSESSMENT_HOST"],
-#         current_app.config["NC_ASSESSMENT_PORT"])
-
-
-# def assessment_uri(
-#         route):
-#     return "http://{}:{}/{}".format(
-#         current_app.config["NC_ASSESSMENT_HOST"],
-#         current_app.config["NC_ASSESSMENT_PORT"],
-#         route)
+def plans_uri(
+        route):
+    route = route.lstrip("/")
+    return "http://{}:{}/{}".format(
+        current_app.config["NC_PLAN_HOST"],
+        current_app.config["NC_PLAN_PORT"],
+        route)
 
 
 @dashboard.route("/")
 def dashboard():
 
-    # api = {
-    #         "resources": {
-    #             # "aggregate_methods": {
-    #             #     "route": "/aggregate_methods"
-    #             # },
-    #             # "aggregate_queries": {
-    #             #     "route": "/aggregate_queries"
-    #             # }
-    #         }
-    #     }
+    plans = []
 
-    # api = json.dumps(api, sort_keys=True, indent=4, separators=(',', ': '))
+    try:
+        uri = plans_uri("plans")
+        response = requests.get(uri)
+        plans = response.json()
+    except Exception as exception:
+        flash("error contacting plan service: {}".format(exception))
 
-    return render_template("index.html")  # , api=api)
+
+    return render_template("index.html",
+        plans=plans)
